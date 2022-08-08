55bd2368cd2d56411ee96b9cfaf0914829ed089b
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2022-08-08 16:52:47 -0300

    feat(notification): :sparkles: implementa teste que retorna todos os erros.

--- src/domain/@shared/notification/notification.ts
+++ src/domain/@shared/notification/notification.ts
@@ -1,12 +1,12 @@
-export type NotificationError = {
+export type NotificationErrorProps = {
   message: string;
   context: string;
 }
 
 export class Notification {
-  private errors: NotificationError[] = [];
+  private errors: NotificationErrorProps[] = [];
 
-  addError(error: NotificationError) {
+  addError(error: NotificationErrorProps) {
     this.errors.push(error);
   }
 
@@ -24,5 +24,9 @@ export class Notification {
 
   hasErrors(): boolean {
     return this.errors.length > 0;
+  }
+
+  getErrors(): NotificationErrorProps[] {
+    return this.errors;
   }
 }
