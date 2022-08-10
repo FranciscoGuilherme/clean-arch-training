560cecf4f1c8b7c425c1d7166261f4232c133bfe
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2022-08-10 17:27:57 -0300

    test(customer): :white_check_mark: implementa o teste sobre a conversao para o XML.

--- src/infrastructure/api/__tests__/customer.e2e.spec.ts
+++ src/infrastructure/api/__tests__/customer.e2e.spec.ts
@@ -30,4 +30,15 @@ describe("E2E test for customer", () => {
     expect(response.body.address.number).toBe(123);
     expect(response.body.address.zip).toBe("12345");
   });
+
+  it("should validate format after customer creation", async () => {
+    const response = await request(app)
+      .get("/customers")
+      .set("Accept", "application/xml")
+      .send();
+
+    expect(response.status).toBe(200);
+    expect(response.text).toContain(`<?xml version="1.0" encoding="UTF-8"?>`);
+    expect(response.text).toContain(`<customers/>`);
+  });
 });
